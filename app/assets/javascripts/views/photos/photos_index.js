c257845c974b8b672d7980bf50a4d1995b4f013c
Karen Ling <karensling@gmail.com>
2015-06-10 17:33:49 -0700

    begin sorting of photos

--- app/assets/javascripts/views/photos/photos_index.js
+++ app/assets/javascripts/views/photos/photos_index.js
@@ -3,15 +3,24 @@ TwinkieSetApp.Views.PhotosIndex = Backbone.CompositeView.extend({
   template: JST['photos/index'],
 
   initialize: function () {
+    this.listenTo(this.collection, 'sync', this.render);
     this.listenTo(this.collection, "add", this.render);
     this.collection.each(this.addPhotoView.bind(this));
   },
 
-  // TODO
+  // TODO able to sort photos
 
-  sortPhotos: function(by) {
-    this.collection.comparator = by;
-  },
+  // events: {
+  //   'click .sort-photo-button': 'sortPhotos'
+  // },
+  //
+  // sortPhotos: function() {
+  //   console.log('sorting');
+  //
+  //   // resortSubviews and pass in css selector and the comparator
+  //   this.collection.comparator = 'image_url';
+  //   this.render();
+  // },
 
   addPhotoView: function (photo) {
     var subview = new TwinkieSetApp.Views.AlbumsShowPhotoItem({
@@ -21,6 +30,7 @@ TwinkieSetApp.Views.PhotosIndex = Backbone.CompositeView.extend({
   },
 
   render: function () {
+    // this.collection.comparator = 'image_url';
     var content = this.template({
       subalbum: this.model
     });
