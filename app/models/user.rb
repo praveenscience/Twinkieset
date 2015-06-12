4e2acf496da88917e80d23a6cb45538d10997b6b
Karen Ling <karensling@gmail.com>
2015-06-12 09:29:09 -0700

    change password string to argument

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,6 +50,6 @@ class User < ActiveRecord::Base
 
   def password=(password)
     @password = password
-    self.password_digest = BCrypt::Password.create("password")
+    self.password_digest = BCrypt::Password.create(password)
   end
 end
