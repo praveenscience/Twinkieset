b12773cc4cb0a435b3cfaa917cc094bb9956fd1c
Karen Ling <karensling@gmail.com>
2015-06-14 01:15:13 -0700

    Put redirect if not logged in back in.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,12 @@
 class StaticPagesController < ApplicationController
 
   def root
-    # redirect_to new_session_url unless logged_in?
+    redirect_to new_session_url unless logged_in?
   end
 
 
   def public_index
+
   end
 
 end
