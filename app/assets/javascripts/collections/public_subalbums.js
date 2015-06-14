e6c56a37eae87a251bb7bf107afd58c31a90b6ff
Karen Ling <karensling@gmail.com>
2015-06-14 02:13:01 -0700

    Add a getOrFech for PublicSubalbums.
    May not need this since we won't be posting, destroying, or putting.

--- app/assets/javascripts/collections/public_subalbums.js
+++ app/assets/javascripts/collections/public_subalbums.js
@@ -7,5 +7,24 @@ TwinkieSetApp.Collections.PublicSubalbums = Backbone.Collection.extend({
   initialize: function (models, options) {
     this.album = options.album;
     this.userID = options.userID;
+  },
+
+  getOrFetch: function (id) {
+    var subalbum = this.get(id);
+
+    if (subalbum) {
+      subalbum.fetch();
+    } else {
+      subalbum = new TwinkieSetApp.Models.PublicSubalbum({
+        userID: this.userID
+      });
+      subalbum.fetch({
+        success: function () {
+          this.add(subalbum);
+        }.bind(this)
+      });
+    }
+
+    return subalbum;
   }
 });
