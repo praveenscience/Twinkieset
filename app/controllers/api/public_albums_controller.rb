871ff97566daa7fa46537188af0c8be8f61ee29c
Karen Ling <karensling@gmail.com>
2015-06-15 00:57:52 -0700

    Remove lines.

--- app/controllers/api/public_albums_controller.rb
+++ app/controllers/api/public_albums_controller.rb
@@ -3,8 +3,7 @@ class Api::PublicAlbumsController < ApplicationController
   def index
     @owner = User.find_by(id: params[:user_id])
     @albums = Album.where(user_id: params[:user_id])
-
     render :index
-
   end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,13 +21,11 @@ class User < ActiveRecord::Base
     dependent: :destroy
   )
 
-
   attr_reader :password
   validates :email, :password_digest, :session_token, presence: true
   validates :email, :password_digest, :session_token, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
   after_initialize :ensure_session_token
-
 
   def ensure_session_token
     self.session_token ||= SecureRandom::urlsafe_base64
