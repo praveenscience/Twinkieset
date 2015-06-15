a0418ff2df8febabe1b55eb904256cbb2b3730f7
Karen Ling <karensling@gmail.com>
2015-06-15 02:41:33 -0700

    Clear out masonry if subalbum is clicked.

--- app/assets/javascripts/views/public/03_public_subalbum_item.js
+++ app/assets/javascripts/views/public/03_public_subalbum_item.js
@@ -22,13 +22,15 @@ TwinkieSetApp.Views.PublicSubalbumItem = Backbone.View.extend({
     $('.list-of-subalbums li').removeClass("selected-subalbum");
     this.$el.addClass("selected-subalbum");
     // alert('something!');
+    TwinkieSetApp.Views.masonryGallery.masonry('destroy');
+    TwinkieSetApp.Views.masonryGallery = null;
+    $('.public-subalbum-photos').html("");
+
+
     var photosInSubalbum = new TwinkieSetApp.Views.PublicSubalbumPhotos({
       model: currentSub
     });
     $('.public-photos-index').html(photosInSubalbum.render().$el);
-
-
-
 
     // TODO implemnt backbone navigate trigger with assocation instead ??? how??? don't want to pass the user_id if not necessary.
     var user_id = this.user_id;
@@ -37,13 +39,6 @@ TwinkieSetApp.Views.PublicSubalbumItem = Backbone.View.extend({
     Backbone.history.navigate("#public/" + user_id + "/collection/" + collection_id + "/set/" + set_id);
 
 
-
-    // $(document).ready(function() {
-    // TODO ugly setTimeout. WELPS
-    // setTimeout(function () {
-    //   this.addMasonry.call(this);
-    // }.bind(this), 10);
-    // }.bind(this));
   },
 
   render: function () {
@@ -54,36 +49,9 @@ TwinkieSetApp.Views.PublicSubalbumItem = Backbone.View.extend({
     this.$el.html(content);
     this.$el.addClass('subalbum-' + this.model.id);
 
+
     return this;
   },
 
 
-  addMasonry: function (event) {
-    var sizing = function () {
-      // console.log('something');
-      var windowWidth = $(window).width() - 48;
-      // console.log(windowWidth);
-
-      if (windowWidth > 1040) {
-        $('.grid-item').css('width', (windowWidth-18)/4);
-      } else if (windowWidth > 820) {
-        $('.grid-item').css('width', (windowWidth-12)/3);
-      } else if (windowWidth > 600) {
-        $('.grid-item').css('width', (windowWidth-6)/2);
-      } else {
-        $('.grid-item').css('width', (windowWidth));
-      }
-    };
-
-    $(window).resize(function () {
-      sizing();
-    });
-
-
-    sizing();
-    $('.public-subalbum-photos').masonry({
-      itemSelector: '.grid-item',
-      gutter: 6
-    });
-  },
 });
