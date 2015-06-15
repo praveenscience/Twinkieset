7c915b881ef1864a2592d6e0a8695a8d6a34c39d
Karen Ling <karensling@gmail.com>
2015-06-15 11:54:17 -0700

    Pass in owner to each album.

--- app/assets/javascripts/views/public/00_public_album_index.js
+++ app/assets/javascripts/views/public/00_public_album_index.js
@@ -3,17 +3,23 @@ TwinkieSetApp.Views.PublicAlbumIndex = Backbone.CompositeView.extend({
 
   initialize: function (options) {
     this.listenTo(this.model, "sync", this.render);
-    this.listenTo(this.model.albums(), 'add', this.addAlbumView);
-    this.model.albums().each(this.addAlbumView.bind(this));
+
+
+
 
   },
 
   addAlbumView: function (album) {
-    var subview = new TwinkieSetApp.Views.PublicAlbumItem({ model: album });
+    var subview = new TwinkieSetApp.Views.PublicAlbumItem({
+      model: album,
+      owner: this.model
+    });
     this.addSubview('.albums-container', subview);
   },
 
   render: function () {
+    this.listenTo(this.model.albums(), 'add', this.addAlbumView);
+    this.model.albums().each(this.addAlbumView.bind(this));
     var content = this.template({
       owner: this.model
     });
