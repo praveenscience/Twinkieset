3c52fe552bedc81d03bb56931350aa98e3d6f1c6
Karen Ling <karensling@gmail.com>
2015-06-15 11:54:49 -0700

    Pass in owner for album item.

--- app/assets/javascripts/views/public/01_public_album_item.js
+++ app/assets/javascripts/views/public/01_public_album_item.js
@@ -2,20 +2,20 @@ TwinkieSetApp.Views.PublicAlbumItem = Backbone.View.extend({
   template: JST['public/01_public_album_item'],
   className: 'one-album col-xs-4 col-sm-4',
 
-  intialize: function () {
-
+  initialize: function (options) {
+    this.owner = options.owner;
     // this.listenTo(this.model, "sync", this.render);
   },
 
 
   render: function () {
+
     var content = this.template({
-      album: this.model
+      album: this.model,
+      owner: this.owner
     });
 
     this.$el.html(content);
-
-
     var cover_image = this.model.get("cover_image_url");
 
     this.$el.find('.album-image').css('background', 'url('+cover_image+')');
