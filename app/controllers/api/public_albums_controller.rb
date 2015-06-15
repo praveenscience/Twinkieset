ec7871bb692ec5cbf7c3f4a927e39616f6cece19
Karen Ling <karensling@gmail.com>
2015-06-15 15:46:22 -0700

    Before action to ensure guest has correct session password.

--- app/controllers/api/public_albums_controller.rb
+++ app/controllers/api/public_albums_controller.rb
@@ -1,9 +1,27 @@
 class Api::PublicAlbumsController < ApplicationController
+  before_action :must_be_logged_in, only: [:show]
 
   def index
-    @owner = User.find_by(id: params[:user_id])
     @albums = Album.where(user_id: params[:user_id])
+    # TODO: Use user.albums association
     render :index
   end
-  
+
+  def show
+    @album = Album.find(params[:id])
+    render :show
+  end
+
+
+  private
+    def must_be_logged_in
+      if Album.find(params[:id]).password_digest
+        if current_album.id != params[:id]
+          # render "Blah", status: :forbidden
+            render text: "None!", status: :forbidden
+          return
+        end
+      end
+
+    end
 end
