ce4f160cb3f1ab8940705a5a47a6a7377fd55c4c
Karen Ling <karensling@gmail.com>
2015-06-15 15:42:22 -0700

    Set up album login.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
-  helper_method :logged_in?, :current_user
+  helper_method :logged_in?, :current_user, :current_album, :logged_in_album?
 
   def current_album
-    album = Album.find_by(album_session_token: session[:album_session_token])
+    album = Album.find_by(password_digest: session[:album_password])
   end
 
   def logged_in_album?
@@ -13,16 +13,10 @@ class ApplicationController < ActionController::Base
   end
 
   def log_into_album!(album)
-    session[:album_session_token] = album.reset_session_token!
-  end
-
-  def log_out_album!
-    current_album.reset_session_token!
-    session[:album_session_token] = nil
+    session[:album_password] = album.password_digest
   end
 
 
-  
   def current_user
     user = User.find_by(session_token: session[:session_token])
   end
