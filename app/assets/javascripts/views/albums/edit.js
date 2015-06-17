66b8dce8f421f0d7a7a30e3e75cd6499ab73eb22
Karen Ling <karensling@gmail.com>
2015-06-17 12:59:24 -0700

    Move album status styling to edit view.

--- app/assets/javascripts/views/albums/edit.js
+++ app/assets/javascripts/views/albums/edit.js
@@ -6,9 +6,28 @@ TwinkieSetApp.Views.EditForm = Backbone.View.extend({
 
   },
 
+
+
+
+
+
   events: {
     "click .submit-edit-album": "editAlbum",
-    "click .cancel-edit-album": "hideForm"
+    "click .cancel-edit-album": "hideForm",
+    'click .status-published': 'setStatusPublished',
+    'click .status-draft': 'setStatusDraft'
+  },
+
+  setStatusPublished: function () {
+    this.$el.find('#album-status').val("Published");
+    this.$el.find('.status-published').addClass('selected');
+    this.$el.find('.status-draft').removeClass('selected');
+  },
+
+  setStatusDraft: function () {
+    this.$el.find('#album-status').val("Draft");
+    this.$el.find('.status-published').removeClass('selected');
+    this.$el.find('.status-draft').addClass('selected');
   },
 
   hideForm: function () {
@@ -34,7 +53,23 @@ TwinkieSetApp.Views.EditForm = Backbone.View.extend({
       album: this.model
     });
     this.$el.html(content);
+    this.onRender();
     return this;
+  },
+
+  onRender: function () {
+    if (this.model.get('status') === 'Draft') {
+      this.$el.find('.status-published').removeClass('selected');
+      this.$el.find('.status-draft').addClass('selected');
+    } else {
+      this.$el.find('.status-published').addClass('selected');
+      this.$el.find('.status-draft').removeClass('selected');
+
+    }
+
+
+
+
   }
 
 });
