7b09e33d0910d57fa9d4476290c96b3db72ab0b0
Karen Ling <karensling@gmail.com>
2015-06-17 13:00:26 -0700

    Only display published albums for user.

--- app/controllers/api/public_users_controller.rb
+++ app/controllers/api/public_users_controller.rb
@@ -2,7 +2,7 @@ class Api::PublicUsersController < ApplicationController
 
   def index
     @owner = User.find_by(id: params[:user_id])
-    @albums = @owner.albums
+    @albums = @owner.albums.where(status: 'Published')
     render :index
   end
 
