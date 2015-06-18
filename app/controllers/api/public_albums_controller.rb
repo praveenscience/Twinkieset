62104e367761f72441d95a135364b593c7f0edba
Karen Ling <karensling@gmail.com>
2015-06-18 01:54:59 -0700

    Allow empty password and comment out public albums index, which is unused.

--- app/controllers/api/public_albums_controller.rb
+++ app/controllers/api/public_albums_controller.rb
@@ -1,11 +1,12 @@
 class Api::PublicAlbumsController < ApplicationController
   before_action :must_be_logged_in, only: [:show]
 
-  def index
-    @albums = Album.where(user_id: params[:user_id])
-    # TODO: Use user.albums association
-    render :index
-  end
+  # def index
+  #   @user = User.find(params[:user_id])
+  #   @albums = @user.albums
+  #   # TODO: Use user.albums association
+  #   render :index
+  # end
 
   def show
     @album = Album.find(params[:id])
@@ -16,7 +17,9 @@ class Api::PublicAlbumsController < ApplicationController
   private
     def must_be_logged_in
       if Album.find(params[:id]).password_digest
-        if current_album.id.to_i != params[:id].to_i
+        if Album.find(params[:id]).is_password?("")
+          return
+        elsif current_album.id.to_i != params[:id].to_i
           # render "Blah", status: :forbidden
           # redirect_to new_album_session_url
             render text: "None!", status: :forbidden
