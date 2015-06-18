868f43476d36cb54b5344b8c906c9e98b7759507
Karen Ling <karensling@gmail.com>
2015-06-18 06:48:31 -0700

    Implement log out button.

--- app/assets/javascripts/views/shared/navbar.js
+++ app/assets/javascripts/views/shared/navbar.js
@@ -2,6 +2,15 @@ TwinkieSetApp.Views.NavBar = Backbone.View.extend({
   template: JST['shared/navbar'],
   className: "navibar",
 
+
+  events: {
+    'click .logout': 'logoutUser'
+  },
+
+  logoutUser: function (event) {
+    $('.logout-form').submit();
+  },
+
   render: function () {
     var content = this.template();
     this.$el.html(content);
