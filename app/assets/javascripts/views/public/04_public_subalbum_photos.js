fecf343c376ccb90c302760744d203a71216b6bb
Karen Ling <karensling@gmail.com>
2015-06-18 09:01:11 -0700

    Remove comments.

--- app/assets/javascripts/views/public/04_public_subalbum_photos.js
+++ app/assets/javascripts/views/public/04_public_subalbum_photos.js
@@ -84,18 +84,13 @@ TwinkieSetApp.Views.PublicSubalbumPhotos = Backbone.CompositeView.extend({
 
     $('#container').imagesLoaded()
       .always( function( instance ) {
-        console.log('all images loaded');
         this.addMasonry();
       }.bind(this))
       .done( function( instance ) {
-        console.log('all images successfully loaded');
       })
       .fail( function() {
-        console.log('all images loaded, at least one is broken');
       })
       .progress( function( instance, image ) {
-        var result = image.isLoaded ? 'loaded' : 'broken';
-        console.log( 'image is ' + result + ' for ' + image.img.src );
       });
   },
 
