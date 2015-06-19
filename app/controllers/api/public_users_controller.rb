3af461fca1078b31cfd016b1a182d261d12a0fa3
Karen Ling <karensling@gmail.com>
2015-06-18 21:30:47 -0700

    Create users controller index to add examples.

--- app/controllers/api/public_users_controller.rb
+++ app/controllers/api/public_users_controller.rb
@@ -1,5 +1,10 @@
 class Api::PublicUsersController < ApplicationController
 
+  def index
+    @users = User.all
+    render :index
+  end
+
   def show
     @owner = User.find_by(id: params[:user_id])
     @albums = @owner.albums.where(status: 'Published')
