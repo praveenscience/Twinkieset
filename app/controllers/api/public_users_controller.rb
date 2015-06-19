e7a313dd9d03daac0e5e983348e480e4910c8908
Karen Ling <karensling@gmail.com>
2015-06-19 10:48:47 -0700

    Remove index.

--- app/controllers/api/public_users_controller.rb
+++ app/controllers/api/public_users_controller.rb
@@ -1,14 +1,7 @@
 class Api::PublicUsersController < ApplicationController
-
-  def index
-    @users = User.all
-    render :index
-  end
-
   def show
     @owner = User.find_by(id: params[:user_id])
     @albums = @owner.albums.where(status: 'Published')
     render :show
   end
-
 end
