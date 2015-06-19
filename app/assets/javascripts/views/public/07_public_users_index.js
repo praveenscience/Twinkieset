0eacaed8effc92023b982b56c295d421e31e2cb7
Karen Ling <karensling@gmail.com>
2015-06-19 10:52:05 -0700

    Remove backbone views for public users. Moved to front page to make one page.

--- app/assets/javascripts/views/public/07_public_users_index.js
+++
@@ -1,9 +0,0 @@
-TwinkieSetApp.Views.PublicUsersIndex = Backbone.View.extend({
-  template: JST['public/07_public_users_index'],
-
-  render: function () {
-    var content = this.template();
-    this.$el.html(content);
-    return this;
-  }
-});
