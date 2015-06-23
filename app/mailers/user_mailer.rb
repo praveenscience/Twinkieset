6b8c2dbcab46a79673eefeaf2030df2a8d9a772b
Karen Ling <karensling@gmail.com>
2015-06-22 20:45:28 -0700

    Update email subject.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,12 +4,12 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url = new_session_url
-    mail(to: user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: user.email, subject: 'Welcome to Twinkieset.com')
   end
 
   def reset_password(user)
     @user = user
-    mail(to: user.email, subject: "Reset your password.")
+    mail(to: user.email, subject: "Reset your password for Twinkieset.com")
   end
 
 end
