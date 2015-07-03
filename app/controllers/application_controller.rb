79df7b8a426aaed9e68fff5211605d3131a893f2
Karen Ling <karensling@gmail.com>
2015-07-02 19:43:04 -0700

    Change CSRF protection to null_session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session,
+      if: Proc.new { |c| c.request.format =~ %r{application/json} }
   helper_method :logged_in?, :current_user, :current_album, :logged_in_album?
 
   def current_album
